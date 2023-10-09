1a99929f45abead376c8dc199ae90f769edc22ea
latan <latand666@gmail.com>
2023-10-09 23:25:10 +0300

    Add Telegram version check and notify user to update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,40 +11,45 @@ import SlotSelection from "./pages/appointment-booking.jsx";
 import RegistrationConfirmation from "./pages/successful-booking.jsx";
 import ClinicSelection from "./pages/clinic-selection.jsx";
 import FullSummary from "./pages/booking-resume.jsx";
+import {useShowPopup} from "@vkruglikov/react-telegram-web-app";
 
 const App = () => {
+    const showPopup = useShowPopup();
     useEffect(() => {
         if (window.Telegram && window.Telegram.WebApp) {
+            if (!window.Telegram.WebApp.isVersionAtLeast("6.9")) {
+                showPopup({message: "Please update your Telegram app to use this app."});
+            }
             // Alternatively to what can be set with react-telegram-web-app, you can directly set the following properties:
             window.Telegram.WebApp.expand();
         }
     }, []);
 
     return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<LandingPage/>}/>
-                    <Route path="/see_a_doctor" element={<DoctorSelection/>}/>
-                    <Route path="/get_tested" element={<GetTested/>}/>
-                    <Route path="/doctor/:doctor_id" element={<About/>}/>
-                    <Route
-                        path="/booking/appointment"
-                        element={<SlotSelection
-                            storageKey="selectedDoctor"
-                            itemType="doctors"
-                        />}/>
-                    <Route path="/booking/diagnostics/:diagnostic_id" element={<ClinicSelection/>}/>
-                    <Route path="/booking/clinic" element={<SlotSelection
-                        storageKey="selectedDiagnostic"
-                        itemType="diagnostics"
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LandingPage/>}/>
+                <Route path="/see_a_doctor" element={<DoctorSelection/>}/>
+                <Route path="/get_tested" element={<GetTested/>}/>
+                <Route path="/doctor/:doctor_id" element={<About/>}/>
+                <Route
+                    path="/booking/appointment"
+                    element={<SlotSelection
+                        storageKey="selectedDoctor"
+                        itemType="doctors"
                     />}/>
-                    <Route path="/booking/patient-info-form/:itemType" element={<PatientInformation/>}/>
+                <Route path="/booking/diagnostics/:diagnostic_id" element={<ClinicSelection/>}/>
+                <Route path="/booking/clinic" element={<SlotSelection
+                    storageKey="selectedDiagnostic"
+                    itemType="diagnostics"
+                />}/>
+                <Route path="/booking/patient-info-form/:itemType" element={<PatientInformation/>}/>
 
-                    <Route path="/booking/confirmation/:itemType" element={<FullSummary/>}/>
+                <Route path="/booking/confirmation/:itemType" element={<FullSummary/>}/>
 
-                    <Route path="/successful_booking" element={<RegistrationConfirmation/>}/>
-                </Routes>
-            </BrowserRouter>
+                <Route path="/successful_booking" element={<RegistrationConfirmation/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
