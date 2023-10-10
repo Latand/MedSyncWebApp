511dfefcbc4966e038dda184f26afb78faca26a8
latan <latand666@gmail.com>
2023-10-10 18:01:57 +0300

    update vite config to allow automatic refresh if files are updated in dev mode

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true, // Here
+        strictPort: true,
+        port: 5173,
+    }
 })
